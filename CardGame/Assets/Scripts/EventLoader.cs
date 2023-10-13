297b77b3e9c5834c315401136a7ffbc6c4330e79
Gopherdays <g4r64g3m4n@gmail.com>
2023-10-13 08:41:24 -0700

    I commented the EventLoader script :)

--- CardGame/Assets/Scripts/EventLoader.cs
+++ CardGame/Assets/Scripts/EventLoader.cs
@@ -31,16 +31,19 @@ public class EventLoader : MonoBehaviour
     //this tracks how long it's been since the last ambush event
     public int timesSinceAmbush;
 
+    //this bool makes it so it doesn't load new events every frame
     public bool eventDecided = false;
 
+    //this stores the random value
     private int randomness;
 
+    //this increases the chance of a fork every time there's not a fork
     private int chanceIncrease;
-
-    private bool wasFork = false;
 
     void Start()
     {
+        //intitializing vars
+        //this makes it so there isn't 3 non-fight events in a row at the beginning of a map
         timesSinceAmbush = 1;
         chanceIncrease = 0;
     }
@@ -48,13 +51,16 @@ public class EventLoader : MonoBehaviour
 
     void Update()
     {
+        //this debug lets me test the random logic
         if (Input.GetKeyDown(KeyCode.G))
         {
             ChooseEvent();
             if (!fork || nextEvent == "Fight")
+                //if there isn't a fork, sya this \/
                 Debug.Log("The next event is " + nextEvent);
             else if (fork && nextEvent != "Fight")
             {
+                //if there is a fork, say this \/
                 Debug.Log("There is a fork in the road!");
                 Debug.Log("The first option is " + nextEvent);
                 Debug.Log("The second option is " + secondEvent);
@@ -63,78 +69,95 @@ public class EventLoader : MonoBehaviour
 
             
     }
+    //put this in a function to make it easier
     private void ChooseEvent()
     {
+        //sets fork to false at the beginning so that debug works
         fork = false;
 
+        //the random value for the first event
         randomness = Random.Range(1, 100);
         Debug.Log("first event rando: " + randomness);
 
+        //the random value for forking - the chance increase
         int forkDecider = (Random.Range(1, 100) - chanceIncrease);
         Debug.Log("forkDecider Rando: " + forkDecider);
 
+        //makes fork true at a specific number
         if (forkDecider <= 30)
             fork = true;
 
+        //fights happen every 3 events
         if (timesSinceAmbush >= 3)
         {
             nextEvent = "Fight";
             timesSinceAmbush = 0;
         }
+
+        //if it's not a fight, generate random event
         else
         {
+            //30% chance of the Item event
             if (randomness >= 1 && randomness <= 30)
             {
                 nextEvent = "Items";
             }
-
+            //30% chance of the Card event
             else if (randomness >= 31 && randomness <= 60)
-            {
-                nextEvent = "Wizard";
-            }
-
-            else if (randomness >= 61 && randomness <= 100)
             {
                 nextEvent = "Cards";
             }
+            //40% chance of the Wizard event
+            else if (randomness >= 61 && randomness <= 100)
+            {
+                nextEvent = "Wizard";
+            }
         }
-        if ((fork || wasFork) && nextEvent != "Fight" )
+
+        //if there is a fork and there isn't a fight, generate a second event
+        if (fork && nextEvent != "Fight" )
         {
+            //new random value
             randomness = Random.Range(1, 100);
+
+            //these lines make sure that there aren't duplicate events in a fork
             if (nextEvent == "Items")
             {
                 if (randomness >= 1 && randomness <= 65)
-                    secondEvent = "Cards";
-                else
                     secondEvent = "Wizard";
-            }
-            else if (nextEvent == "Wizard")
-            {
-                if (randomness >= 1 && randomness <= 65)
-                    secondEvent = "Cards";
                 else
-                    secondEvent = "Items";
+                    secondEvent = "Cards";
             }
             else if (nextEvent == "Cards")
             {
-                if (randomness >= 1 && randomness <= 50)
+                if (randomness >= 1 && randomness <= 65)
                     secondEvent = "Wizard";
                 else
                     secondEvent = "Items";
             }
+            else if (nextEvent == "Wizard")
+            {
+                if (randomness >= 1 && randomness <= 50)
+                    secondEvent = "Cards";
+                else
+                    secondEvent = "Items";
+            }
+
+            //currently unused code for a 3-way fork
             if (forkType)
             {
                 //I'm going to leave this empty until we have more events
             }
+
+            //if there was a fork, reset the chance increase for a fork
             chanceIncrease = 0;
-            wasFork = false;
         }
-        else if (fork && nextEvent == "Fight")
-        {
-            wasFork = true;
-        }
+
+        //if there wasn't a fork, increase the chance of a fork
         else if (!fork)
             chanceIncrease += 5;
+
+        //add one time since the last ambush
         timesSinceAmbush++;
     }
 }
