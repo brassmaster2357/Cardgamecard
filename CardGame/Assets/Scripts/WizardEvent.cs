b89f12d4aadc817bfb62e307c678621ba116baf7
Gopherdays <g4r64g3m4n@gmail.com>
2023-10-18 10:19:35 -0700

    starting wizard event

---
+++ CardGame/Assets/Scripts/WizardEvent.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+okay me, this is how it's going to work.
+you will load in
+then you are able to choose a card
+this card will be buffed by the player's choice of +1 attack or +2 health
+then it will transferr back to the map
+*/
+public class WizardEvent : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
