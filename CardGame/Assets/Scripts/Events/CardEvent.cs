b89c9039e8a84e7a56a52afe49d57797dd80c772
Gopherdays <g4r64g3m4n@gmail.com>
2023-11-01 10:21:33 -0700

    working on the card event

--- CardGame/Assets/Scripts/Events/CardEvent.cs
+++ CardGame/Assets/Scripts/Events/CardEvent.cs
@@ -5,17 +5,48 @@ using UnityEngine;
 public class CardEvent : MonoBehaviour
 {
 
-    private CardTemplate[] cardArray;
+    public CardTemplate[] cardArray;
 
-    private int random;
+    private CardTemplate randomCard;
+
+    public GameObject pch;
+
+    public GameObject cardBase;
+
+    private int cardNum;
 
     void Start()
     {
-        random = Random.Range(1, 13);
+        
+
+        randomCard = cardArray[Random.Range(1, cardArray.Length)];
+        cardNum = -1;
+        LoadCard();
+
+        randomCard = cardArray[Random.Range(1, cardArray.Length)];
+        cardNum = 0;
+        LoadCard();
+
+        randomCard = cardArray[Random.Range(1, cardArray.Length)];
+        cardNum = 1;
+        LoadCard();
     }
 
     void Update()
     {
         
+    }
+
+    void LoadCard()
+    {
+        GameObject cardObject = Instantiate(cardBase, new Vector2(cardNum * 5, 0), Quaternion.identity);
+
+        CardsScript tempScript = cardObject.GetComponent<CardsScript>();
+
+        tempScript.card = randomCard;
+
+        tempScript.LoadCard();
+
+        tempScript.restPosition = cardObject.transform.position;
     }
 }
--- CardGame/Assets/Scripts/Events/WizardEvent.cs
+++ CardGame/Assets/Scripts/Events/WizardEvent.cs
@@ -7,7 +7,7 @@ okay me, this is how it's going to work.
 you will load in
 then you are able to choose a card
 this card will be buffed by the player's choice of +1 attack or +2 health
-then it will transferr back to the map
+then it will transfer back to the map
 */
 public class WizardEvent : MonoBehaviour
 {
